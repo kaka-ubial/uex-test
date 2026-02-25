e96aa799c304a78175ad3768ec96d53ea8c074b6
kakau <karen.upereira@telefonica.com>
2026-02-25 11:43:08 -0300

    fix: removing unused map handler

--- resources/js/components/contact-map.tsx
+++ resources/js/components/contact-map.tsx
@@ -1,15 +1,4 @@
 import { Map, Marker, useMap } from '@vis.gl/react-google-maps';
-import { useEffect } from 'react';
-
-function MapHandler({ lat, lng }: { lat: number; lng: number }) {
-    const map = useMap();
-    useEffect(() => {
-        if (map) {
-            map.panTo({ lat, lng });
-        }
-    }, [lat, lng, map]);
-    return null;
-}
 
 export function ContactMap({ lat, lng }: { lat: number; lng: number }) {
     return (
