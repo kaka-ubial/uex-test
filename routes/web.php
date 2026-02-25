9cdbeffbe5fe774dc70ed360fc39387d1b517624
kakau <karen.upereira@telefonica.com>
2026-02-25 10:09:37 -0300

    refactor: optimizing routes

--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,18 @@ use Laravel\Fortify\Features;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AddressController;
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/api/proxy-cep/{cep}', [AddressController::class, 'proxyCep']);
-});
-
+//Public route
 Route::get('/', function () {
     return Inertia::render('welcome', [
         'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('home');
 
-Route::get('dashboard', [ContactController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::get('/contacts/create', [ContactController::class, 'create'])
-    ->name('contacts.create');
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-
-Route::delete('/contacts/{contact}', [ContactController::class, 'destroy']);
-
-Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
-Route::patch('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
+//Protected routes
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/api/proxy-cep/{cep}', [AddressController::class, 'proxyCep']);
+    Route::get('dashboard', [ContactController::class, 'index'])->name('dashboard');
+    Route::resource('contacts', ContactController::class)->except(['index', 'show']);
+});
 
 require __DIR__.'/settings.php';
